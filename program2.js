80553e257291aea2ddc9cf60e157e96f9c8d26d6
saib-ahmed <saib9564@gamil.com>
2024-10-08 12:18:41 +0530

    program1.js and program2.js solved

--- program2.js
+++ program2.js
@@ -1,7 +1,28 @@
-const decodeTheRing = function (s, p) {
+const decodeTheRing = function(s, p) {
+  const sLen = s.length, pLen = p.length;
+  const dp = Array(sLen + 1).fill(null).map(() => Array(pLen + 1).fill(false));
 
-    // write your code here
+  dp[0][0] = true;
 
-  };
-  
-  module.exports = decodeTheRing;
+  for (let j = 1; j <= pLen; j++) {
+      if (p[j - 1] === '*') {
+          dp[0][j] = dp[0][j - 1];
+      }
+  }
+
+  for (let i = 1; i <= sLen; i++) {
+      for (let j = 1; j <= pLen; j++) {
+          const patternChar = p[j - 1], stringChar = s[i - 1];
+
+          if (patternChar === '?' || patternChar === stringChar) {
+              dp[i][j] = dp[i - 1][j - 1];
+          } else if (patternChar === '*') {
+              dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+          }
+      }
+  }
+
+  return dp[sLen][pLen];
+};
+
+module.exports = decodeTheRing;
